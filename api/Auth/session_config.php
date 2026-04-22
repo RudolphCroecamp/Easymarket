5184b6c661577ff141838a2e888db235e33768b8
RudolphCroecamp <RudolphCroecamp@gmail.com>
2026-04-22 09:55:26 +0200

    fixed cors and session for production

--- api/Auth/session_config.php
+++ api/Auth/session_config.php
@@ -1,11 +1,11 @@
 <?php
-session_set_cookie_params([
-    'lifetime' => 60 * 180,
-    'path' => '/',
-    'domain' => 'localhost',
-    'secure' => false,       
-    'httponly' => true,
-    'samesite' => 'Lax'      
-]);
+    session_set_cookie_params([
+        'lifetime' => 60 * 180,
+        'path' => '/',
+        'domain' => 'easymarket-727523185751.europe-west1.run.app',
+        'secure' => true,
+        'httponly' => true,
+        'samesite' => 'None'
+    ]);
 
-session_start();
+    session_start();
--- config/cors.php
+++ config/cors.php
@@ -1,28 +1,33 @@
 <?php
 
-$allowedOrigins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:8080",
-    "http://localhost:80",
-];
+// $allowedOrigins = [
+//     "http://127.0.0.1:5500",
+//     "http://localhost:5500",
+//     "http://127.0.0.1:8080",
+//     "http://localhost:80",
+// ];
 
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+// $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
-if (in_array($origin, $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: *");//$origin
-    header("Vary: Origin");
-} else {
-    // IMPORTANT: fail-safe for debugging (remove later if needed)
-    header("Access-Control-Allow-Origin: *");
-}
+// if (in_array($origin, $allowedOrigins)) {
+//     header("Access-Control-Allow-Origin: *");//$origin
+//     header("Vary: Origin");
+// } else {
+//     // IMPORTANT: fail-safe for debugging (remove later if needed)
+//     header("Access-Control-Allow-Origin: *");
+// }
 
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Credentials: true");
-header("Content-Type: application/json");
+// header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type");
+// header("Access-Control-Allow-Credentials: true");
+// header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit();
-}
+    header("Access-Control-Allow-Origin: https://easymarket-727523185751.europe-west1.run.app");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(204);
+        exit();
+    }
