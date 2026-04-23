5cdca61ffa9e2a0202df39a0a7e6f411d033e328
RudolphCroecamp <RudolphCroecamp@gmail.com>
2026-04-23 21:36:10 +0200

    rfgty

--- api/improved_messages/newMessage.php
+++ api/improved_messages/newMessage.php
@@ -7,18 +7,18 @@
     error_reporting(E_ALL);
 
     // import env variables
-    try {
-        require_once __DIR__ . '/../../vendor/autoload.php';
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
-        $dotenv->load();
-    } catch (\Throwable $th) {
-        echo json_encode([
-                "status"=>"failed",
-                "success"=>false,
-                "error"=>"Could not load environment variables"
-            ]);
-        die();
-    }
+    // try {
+    //     require_once __DIR__ . '/../../vendor/autoload.php';
+    //     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+    //     $dotenv->load();
+    // } catch (\Throwable $th) {
+    //     echo json_encode([
+    //             "status"=>"failed",
+    //             "success"=>false,
+    //             "error"=>"Could not load environment variables"
+    //         ]);
+    //     die();
+    // }
 
     require '../../config/cors.php';//allow access from webserver
     require '../../config/protectedRoute.php';//user must be authorised
