a2ee7b0ebf492d018950418ab33e62fa8e5a804d
RudolphCroecamp <RudolphCroecamp@gmail.com>
2026-05-13 20:33:31 +0200

    session_config

--- api/Auth/session_config.php
+++ api/Auth/session_config.php
@@ -2,10 +2,10 @@
 session_set_cookie_params([
     'lifetime' => 60 * 180,
     'path' => '/',
-    'domain' => 'localhost',
-    'secure' => false,       
+    'domain' => 'easymarket-727523185751.europe-west1.run.app',
+    'secure' => true,
     'httponly' => true,
-    'samesite' => 'Lax'      
+    'samesite' => 'None'
 ]);
 
 
