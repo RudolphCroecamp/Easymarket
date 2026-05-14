a90e7db5592233e18c87f3c9e0a3ce414e2dba0e
RudolphCroecamp <RudolphCroecamp@gmail.com>
2026-05-14 18:49:27 +0200

    uuyyy

--- api/payfast/paymentNotification.php
+++ api/payfast/paymentNotification.php
@@ -6,142 +6,142 @@
     header( 'HTTP/1.0 200 OK' );
     flush();
 
-    // define( 'SANDBOX_MODE', true );
-    // $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
-    // // Posted variables from ITN
-    // $pfData = $_POST;
+    define( 'SANDBOX_MODE', true );
+    $pfHost = SANDBOX_MODE ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
+    // Posted variables from ITN
+    $pfData = $_POST;
 
-    // // Strip any slashes in data
-    // foreach( $pfData as $key => $val ) {
-    //     $pfData[$key] = stripslashes( $val );
-    // }
+    // Strip any slashes in data
+    foreach( $pfData as $key => $val ) {
+        $pfData[$key] = stripslashes( $val );
+    }
 
-    // $pfParamString = "";
-    // // Convert posted variables to a string
-    // foreach( $pfData as $key => $val ) {
-    //     if( $key !== 'signature' ) {
-    //         $pfParamString .= $key .'='. urlencode( $val ) .'&';
-    //     } else {
-    //         break;
-    //     }
-    // }
+    $pfParamString = "";
+    // Convert posted variables to a string
+    foreach( $pfData as $key => $val ) {
+        if( $key !== 'signature' ) {
+            $pfParamString .= $key .'='. urlencode( $val ) .'&';
+        } else {
+            break;
+        }
+    }
 
-    // $pfParamString = substr( $pfParamString, 0, -1 );
-
-
-
-    // function pfValidSignature( $pfData, $pfParamString, $pfPassphrase = null ) {
-    //     // Calculate security signature
-    //     if($pfPassphrase === null) {
-    //         $tempParamString = $pfParamString;
-    //     } else {
-    //         $tempParamString = $pfParamString.'&passphrase='.urlencode( $pfPassphrase );
-    //     }
-
-    //     $signature = md5( $tempParamString );
-    //     return ( $pfData['signature'] === $signature );
-    // }
+    $pfParamString = substr( $pfParamString, 0, -1 );
 
 
 
-    // function pfValidIP() {
-    //     // Variable initialization
-    //     $validHosts = array(
-    //         'www.payfast.co.za',
-    //         'sandbox.payfast.co.za',
-    //         'w1w.payfast.co.za',
-    //         'w2w.payfast.co.za',
-    //         );
+    function pfValidSignature( $pfData, $pfParamString, $pfPassphrase = null ) {
+        // Calculate security signature
+        if($pfPassphrase === null) {
+            $tempParamString = $pfParamString;
+        } else {
+            $tempParamString = $pfParamString.'&passphrase='.urlencode( $pfPassphrase );
+        }
 
-    //     $validIps = [];
-
-    //     foreach( $validHosts as $pfHostname ) {
-    //         $ips = gethostbynamel( $pfHostname );
-
-    //         if( $ips !== false )
-    //             $validIps = array_merge( $validIps, $ips );
-    //     }
-
-    //     // Remove duplicates
-    //     $validIps = array_unique( $validIps );
-    //     // $referrerIp = gethostbyname(parse_url($_SERVER['HTTP_REFERER'])['host']);
-    //     $remoteIp = $_SERVER['REMOTE_ADDR'] ?? '';
-    //     // if( in_array( $referrerIp, $validIps, true ) ) {
-    //     //     return true;
-    //     // }
-
-    //     return in_array($remoteIp, $validIps, true);
-
-    //     // return false;
-    // }
-
-
-    // function pfValidPaymentData( $cartTotal, $pfData ) {
-    //     return !(abs((float)$cartTotal - (float)$pfData['amount_gross']) > 0.01);
-    // }
+        $signature = md5( $tempParamString );
+        return ( $pfData['signature'] === $signature );
+    }
 
 
 
-    // function pfValidServerConfirmation( $pfParamString, $pfHost = 'sandbox.payfast.co.za', $pfProxy = null ) {
-    //     // Use cURL (if available)
-    //     if( in_array( 'curl', get_loaded_extensions(), true ) ) {
-    //         // Variable initialization
-    //         $url = 'https://'. $pfHost .'/eng/query/validate';
+    function pfValidIP() {
+        // Variable initialization
+        $validHosts = array(
+            'www.payfast.co.za',
+            'sandbox.payfast.co.za',
+            'w1w.payfast.co.za',
+            'w2w.payfast.co.za',
+            );
 
-    //         // Create default cURL object
-    //         $ch = curl_init();
+        $validIps = [];
+
+        foreach( $validHosts as $pfHostname ) {
+            $ips = gethostbynamel( $pfHostname );
+
+            if( $ips !== false )
+                $validIps = array_merge( $validIps, $ips );
+        }
+
+        // Remove duplicates
+        $validIps = array_unique( $validIps );
+        $referrerIp = gethostbyname(parse_url($_SERVER['HTTP_REFERER'])['host']);
+        // $remoteIp = $_SERVER['REMOTE_ADDR'] ?? '';
+        if( in_array( $referrerIp, $validIps, true ) ) {
+            return true;
+        }
+
+        // return in_array($remoteIp, $validIps, true);
+
+        return false;
+    }
+
+
+    function pfValidPaymentData( $cartTotal, $pfData ) {
+        return !(abs((float)$cartTotal - (float)$pfData['amount_gross']) > 0.01);
+    }
+
+
+
+    function pfValidServerConfirmation( $pfParamString, $pfHost = 'sandbox.payfast.co.za', $pfProxy = null ) {
+        // Use cURL (if available)
+        if( in_array( 'curl', get_loaded_extensions(), true ) ) {
+            // Variable initialization
+            $url = 'https://'. $pfHost .'/eng/query/validate';
+
+            // Create default cURL object
+            $ch = curl_init();
         
-    //         // Set cURL options - Use curl_setopt for greater PHP compatibility
-    //         // Base settings
-    //         curl_setopt( $ch, CURLOPT_USERAGENT, NULL );       // Set user agent
-    //         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); // Return output as string rather than outputting it
-    //         curl_setopt( $ch, CURLOPT_HEADER, false );        // Don't include header in output
-    //         curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2 );
-    //         curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, true );
+            // Set cURL options - Use curl_setopt for greater PHP compatibility
+            // Base settings
+            curl_setopt( $ch, CURLOPT_USERAGENT, NULL );       // Set user agent
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true ); // Return output as string rather than outputting it
+            curl_setopt( $ch, CURLOPT_HEADER, false );        // Don't include header in output
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 2 );
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, true );
             
-    //         // Standard settings
-    //         curl_setopt( $ch, CURLOPT_URL, $url );
-    //         curl_setopt( $ch, CURLOPT_POST, true );
-    //         curl_setopt( $ch, CURLOPT_POSTFIELDS, $pfParamString );
-    //         if( !empty( $pfProxy ) )
-    //             curl_setopt( $ch, CURLOPT_PROXY, $pfProxy );
+            // Standard settings
+            curl_setopt( $ch, CURLOPT_URL, $url );
+            curl_setopt( $ch, CURLOPT_POST, true );
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $pfParamString );
+            if( !empty( $pfProxy ) )
+                curl_setopt( $ch, CURLOPT_PROXY, $pfProxy );
         
-    //         // Execute cURL
-    //         $response = curl_exec( $ch );
-    //         curl_close( $ch );
-    //         if (trim($response) === 'VALID'){
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
+            // Execute cURL
+            $response = curl_exec( $ch );
+            curl_close( $ch );
+            if (trim($response) === 'VALID'){
+                return true;
+            }
+        }
+        return false;
+    }
 
     //start transaction
     $conn->begin_transaction();
 
     try {
-        // $check1 = pfValidSignature($pfData, $pfParamString);
-        // $check2 = pfValidIP();
+        $check1 = pfValidSignature($pfData, $pfParamString);
+        $check2 = pfValidIP();
 
         //get total Price from db
-        $paymentID = 65;// $pfData["m_payment_id"];//get paymentID from payfast
-        // $totalPriceStmt = $conn->prepare("SELECT price FROM orders WHERE paymentID = ?");
-        // $totalPriceStmt->bind_param("i", $paymentID);
+        $paymentID = 66;// $pfData["m_payment_id"];//get paymentID from payfast
+        $totalPriceStmt = $conn->prepare("SELECT price FROM orders WHERE paymentID = ?");
+        $totalPriceStmt->bind_param("i", $paymentID);
 
-        // $totalPriceStmt->execute();
+        $totalPriceStmt->execute();
 
-        // $totalPrice_result = $totalPriceStmt->get_result();
+        $totalPrice_result = $totalPriceStmt->get_result();
 
-        // if($totalPrice_result && $totalPrice_result->num_rows > 0){
-        //     $totalPriceData = $totalPrice_result->fetch_assoc();
-        //     $totalPrice = (float)$totalPriceData['price'];
-        // }else{
-        //     throw new Exception("No orders found with provided ID");
-        // }
+        if($totalPrice_result && $totalPrice_result->num_rows > 0){
+            $totalPriceData = $totalPrice_result->fetch_assoc();
+            $totalPrice = (float)$totalPriceData['price'];
+        }else{
+            throw new Exception("No orders found with provided ID");
+        }
 
 
-        // $check3 = pfValidPaymentData($totalPrice, $pfData);
-        // $check4 = pfValidServerConfirmation($pfParamString, $pfHost);
+        $check3 = pfValidPaymentData($totalPrice, $pfData);
+        $check4 = pfValidServerConfirmation($pfParamString, $pfHost);
 
         if(10 > 2) {//$check1 && $check2 && $check3 && $check4
             // All checks have passed, the payment is successful
