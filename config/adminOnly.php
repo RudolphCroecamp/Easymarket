42db680735cd7274d8b313c910e86937dbef12fc
RudolphCroecamp <RudolphCroecamp@gmail.com>
2026-07-01 17:37:40 +0200

    admin table display issue fix

--- config/adminOnly.php
+++ config/adminOnly.php
@@ -1,6 +1,5 @@
 <?php
-    // session_start();
-    require './session_config.php';//set session details
+    // require '../Auth/session_config.php';
 
     if (!isset($_SESSION['role']) || $_SESSION['role'] !== "admin") {
         http_response_code(401);
--- config/protectedRoute.php
+++ config/protectedRoute.php
@@ -1,6 +1,5 @@
 <?php
     require '../Auth/session_config.php';//set session details
-    // ../../config/cors.php
 
     if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
         http_response_code(401);
