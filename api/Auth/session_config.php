b66f9dada2a23e75105ba0d77c187fc6658b8ae0
RudolphCroecamp <RudolphCroecamp@gmail.com>
2026-07-01 15:15:32 +0200

    ee

--- api/Auth/session_config.php
+++ api/Auth/session_config.php
@@ -1,12 +1,12 @@
 <?php
-session_set_cookie_params([
-    'lifetime' => 60 * 180,
-    'path' => '/',
-    // 'domain' => 'localhost',
-    'secure' => false,       
-    'httponly' => true,
-    'samesite' => 'Lax'      
-]);
+// session_set_cookie_params([
+//     'lifetime' => 60 * 180,
+//     'path' => '/',
+//     // 'domain' => 'localhost',
+//     'secure' => false,       
+//     'httponly' => true,
+//     'samesite' => 'Lax'      
+// ]);
 
 session_set_cookie_params([
     'lifetime' => 60 * 180,
